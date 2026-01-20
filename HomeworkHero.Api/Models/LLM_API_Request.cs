7a2f4146487b716ce0cde8c78ab713887901ea75
Andrew V. Grech <andrew.grech@biztech-consulta.com>
2026-01-20 15:16:30 +0100

    added api request model

---
+++ HomeworkHero.Api/Models/LLM_API_Request.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeworkHero.Api.Models
+{
+    public class LLM_API_Request
+    {
+
+        public class Rootobject
+        {
+            public string apiKey { get; set; }
+            public string provider { get; set; }
+            public string model { get; set; }
+            public bool isChat { get; set; }
+            public Chathistory[] chatHistory { get; set; }
+        }
+
+        public class Chathistory
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+        }
+
+    }
+}
