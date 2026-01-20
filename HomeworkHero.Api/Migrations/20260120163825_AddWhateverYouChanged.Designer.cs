4a71e14f4e7ce52f9d05affec0ccb4ea87cc873e
Andrew V. Grech <andrew.grech@biztech-consulta.com>
2026-01-20 17:51:35 +0100

    migrations

--- HomeworkHero.Api/Migrations/20260120163825_AddWhateverYouChanged.Designer.cs
+++ HomeworkHero.Api/Migrations/20260120163825_AddWhateverYouChanged.Designer.cs
@@ -4,6 +4,7 @@ using HomeworkHero.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeworkHero.Api.Migrations
 {
     [DbContext(typeof(HomeworkHeroContext))]
-    partial class HomeworkHeroContextModelSnapshot : ModelSnapshot
+    [Migration("20260120163825_AddWhateverYouChanged")]
+    partial class AddWhateverYouChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HomeworkHero.Api/Migrations/20260120163825_AddWhateverYouChanged.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeworkHero.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWhateverYouChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paramiters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paramiters", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paramiters_Name",
+                table: "Paramiters",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Paramiters");
+        }
+    }
+}
